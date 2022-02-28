f7c4ec72cfba5b10a9187affe1028a9a5b68e2a4
Ronald Du <75736373+baconisfum@users.noreply.github.com>
2022-02-27 22:17:27 -0800

    Delete Compare.py

--- Compare.py
+++
@@ -1,9 +0,0 @@
-# -------------------------------------------------------------------
-#
-#   Compare.py
-#
-#   Purpose: Allows for the comparison and difference   
-#   identificiation between two network topologies  
-#
-# -------------------------------------------------------------------
-
