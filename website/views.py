436f7509e61509ef0eb36530652193e152a23a37
claire_gavin <66581796+claire-gavin@users.noreply.github.com>
2022-03-06 20:38:59 -0800

    Update Uploading and Exporting Files

--- website/views.py
+++ website/views.py
@@ -6,26 +6,46 @@
 
 from django.shortcuts import render
 from website.python.DeviceDiscovery import *
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 import json
 
-TESTING = True
+TESTING = False
 
 def home(request):
     return render(request, 'home.html')
 
 def upload(request):
+    
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name.replace(' ', ''), myfile)
+        uploaded_file_url = fs.url(filename)
+        with open('currentfiledirectory.txt', 'w') as f:
+            f.write(str(uploaded_file_url))
+            
     return render(request, 'upload.html')
     
 def inspect(request):
 
     if not TESTING:
+        with open('currentfiledirectory.txt') as f:
+            currentFileDirectory = f.readlines()
+        
+        fileDirectory = currentFileDirectory[0].lstrip('/')
 
-     
-        devices_dict = importDeviceData("test.json")       
-        nodes = getNodes(devices_dict)
-        edges = getEdges(devices_dict)
+        if fileDirectory != "":
+            with open(fileDirectory) as json_file:
+                nodeData = json.load(json_file)
 
-        return render(request, 'inspect.html', {'nodes': json.dumps(nodes), 'edges': json.dumps(edges)})
+            devices_dict = importDeviceData(fileDirectory)       
+            nodes = getNodes(devices_dict)
+            edges = getEdges(devices_dict)
+
+            return render(request, 'inspect.html', {'nodes': json.dumps(nodes), 'edges': json.dumps(edges), 'nodeData': json.dumps(nodeData)})
+        else:
+            return render(request, 'upload.html')
     else:
         with open("test.json") as json_file:
             nodeData = json.load(json_file)
@@ -47,3 +67,4 @@ def export(request):
 
 def help(request):
     return render(request, 'help.html')
+    
